dcef0e271436e8e1beac77ee0e82ba7fe73155a5
Luis Manzo <manzosk3@gmail.com>
2022-05-26 17:05:18 -0500

    pair programming with Angel Felix

--- js/creditCheck.js
+++ js/creditCheck.js
@@ -1,2 +1,25 @@
-exports.creditCheck = function(str) {
+function creditCheck(str) {
+  let arr = str.split('').reverse(); 
+  for(let i = 1; i < arr.length; i+=2) { 
+    arr[i] *= 2
+    let temp = [];
+    if(arr[i] > 9) {
+       temp = arr[i].toString().split('');
+       let sum = temp.reduce((a, b) => {
+          return parseInt(a)+parseInt(b);
+       });
+       arr[i] = sum;
+    }
+  }
+  let res = arr.reduce((a, b) => {
+    return parseInt(a) + parseInt(b);
+  })
+  console.log(res);
+  return (res % 10 === 0)
 }
+
+console.log(creditCheck('5541808923795240'));
+console.log(creditCheck("6011797668867828"));
+console.log(creditCheck("5541801923795240"));
+
+
