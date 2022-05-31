5bf2dc6b76f55b6cb92d408d26ded62838bbee5b
afelix997 <afelix2013@gmail.com>
2022-05-30 17:41:15 -0700

    answer

--- python/credit_check.py
+++ python/credit_check.py
@@ -1,8 +1,32 @@
-def credit_check(str):
-    pass
+from multiprocessing.connection import answer_challenge
 
-# Your Luhn Algorithm Here
-# Expected Output:
-# If it is valid, print "The number is valid!"
-# If it is invalid, print "The number is invalid!"
+def getSum(n):
+    sum = 0
+    for digit in str(n): 
+      sum += int(digit)      
+    return sum
+def credit_check(str):
+    total1=0
+    total2=0
+    rev_1= list(str[-1::-2])
+    rev_2= list(str[-2::-2])
+    multiplied = [2*int(number)  for number in rev_2]
+    rev_2_fixed=[]
+    for value in multiplied:
+        if value > 9:
+           value= getSum(value)
+        rev_2_fixed.append(value)
+    for val in rev_1:
+        total1 = total1 + int(val)
+    for val in rev_2_fixed:
+        total2 = total2 + int(val)
+    if (total1+total2) % 10 == 0:
+        return "The number is valid!"
+    else:
+        return "The number is invalid!"    
+
+    return answer
+
+
+
 
